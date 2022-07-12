8e13f7aa5e32b7d32fafc746c2590859efd37e5e
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-07-12 11:12:20 +0530

    equal sum partition memoized solution

---
+++ src/zerooneknapsack/variations/equalsumpartition/EqualSumPartitionMemoized.java
@@ -0,0 +1,35 @@
+package zerooneknapsack.variations.equalsumpartition;
+
+import java.util.Arrays;
+
+public class EqualSumPartitionMemoized {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 11, 5};
+        int n = arr.length;
+        System.out.println(findPartition(arr, n));
+    }
+
+    static Boolean findPartition(int[] arr, int n) {
+        int arrSum = 0;
+        for (int x : arr)
+            arrSum += x;
+        if (arrSum % 2 != 0)
+            return false; // there is no way equal sum partition exists in case arrSum is odd
+        Boolean[][] t = new Boolean[n + 1][arrSum + 1];
+        for (Boolean[] row : t)
+            Arrays.fill(row, false);
+        return subsetSum(arr, arrSum / 2, n, t);
+    }
+
+    static Boolean subsetSum(int[] arr, int sum, int n, Boolean[][] t) {
+        if (n == 0)
+            return false;
+        if (sum == 0)
+            return true;
+        if (t[n][sum]) // if not false
+            return t[n][sum];
+        if (arr[n - 1] <= sum)
+            return t[n][sum] = subsetSum(arr, sum - arr[n - 1], n - 1, t) || subsetSum(arr, sum, n - 1, t);
+        else return t[n][sum] = subsetSum(arr, sum, n - 1, t);
+    }
+}
